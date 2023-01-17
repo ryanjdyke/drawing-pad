7deda363a48c86314029bde33004990d440f737b
Ryan Dyke <ryanjdyke@gmail.com>
2023-01-17 04:38:07 -0800

    Add grid resizer

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const container = document.querySelector("#container");
-const numPixels = 4096;
 const penColor = "rgb(60,60,60)";
+const gridSizer = document.querySelector(".gridSizer");
+const gridSizeDisplay = document.querySelector("p.gridSizer");
+
+let pixelHeightAndWidth = 10;
+let numPixels = 4096;
 
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+document.body.addEventListener("mousedown", () => (mouseDown = true));
+document.body.addEventListener("mouseup", () => (mouseDown = false));
 
 function pushPixels() {
     for (let i = 0; i < numPixels; i++) {
         const pixel = document.createElement("div");
-        pixel.setAttribute("id", "pixel");
+        pixel.setAttribute("id", "pixel");  
+        pixel.style.backgroundColor = container.style.backgroundColor;
+        pixel.style.width = `${pixelHeightAndWidth}px`;
+        pixel.style.height = `${pixelHeightAndWidth}px`;
         pixel.addEventListener("click", () => (pixel.style.backgroundColor = penColor));
         pixel.addEventListener("mouseover", () => {
             if (!mouseDown) return;
@@ -17,7 +24,38 @@ function pushPixels() {
         });
         container.appendChild(pixel);
     }
+    console.log(pixel.style.width);
 }
+
+// Need to fix this while loop somehow
+function clearPixels() {
+    // const oldPixels = document.querySelectorAll("#pixel");
+    // console.log(oldPixels.length);
+    // for (let i = 0; i < oldPixels.length; i++) {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.children[0]);
+    }
+}
+
+gridSizer.addEventListener("mousemove", () => {
+    gridSizeDisplay.textContent = `${gridSizer.value} X ${gridSizer.value}`;
+});
+
+gridSizer.addEventListener("change", () => {
+    numPixels = (gridSizer.value * gridSizer.value);
+    pixelHeightAndWidth = (640 / gridSizer.value);
+    console.log(pixelHeightAndWidth)
+    clearPixels();
+    pushPixels();
+});
+
+gridSizeDisplay.textContent = `${gridSizer.value} X ${gridSizer.value}`;
 
 pushPixels();
 
+// TODO:
+// Add Clear Button
+// Add Color Picker
+// Use Grid for pixels
+// Add Eraser
+// Add Shading
